fb283089fec506edfb7d39af8bf70b9dbc732484
tsbarrett89 <tsbarrett89@gmail.com>
2020-03-19 15:19:21 -0600

    added removeFeature action to addedFeatures.js, passed down to addedFeature.js, and added onClick to x button

--- src/components/AddedFeatures.js
+++ src/components/AddedFeatures.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import AddedFeature from './AddedFeature';
+import { removeFeature } from '../actions/featuresActions';
 
 const AddedFeatures = props => {
   return (
@@ -10,7 +11,7 @@ const AddedFeatures = props => {
       {props.car.features.length ? (
         <ol type="1">
           {props.car.features.map(item => (
-            <AddedFeature key={item.id} feature={item} />
+            <AddedFeature key={item.id} feature={item} removeFeature={props.removeFeature} />
           ))}
         </ol>
       ) : (
@@ -26,4 +27,4 @@ const mapStateToProps = state => {
   }
 }
 
-export default connect(mapStateToProps, {})(AddedFeatures);
+export default connect(mapStateToProps, { removeFeature })(AddedFeatures);
